0fa1672e181cd639f26e0cc4cccc0b8d170305e2
Lythi45 <scala.bine@gmail.com>
2019-12-09 16:17:24 +0100

    day09

--- day09.py
+++ day09.py
@@ -6,14 +6,14 @@ def genComp(program):
         po=pol[0]
         relb=pol[1]
         def store(off,value):
-            num[[num[po+3],0,relb+num[po+3]][(modi//10**off)%10]]=value
+            num[[num[po+off],-1,relb+num[po+off]][(modi//10**off)%10]]=value
+        def load(off):
+            return num.get([num[po+off],po+off,relb+num[po+off]] [(modi//10**off)%10],0)
         while num[po]!=99:
             comm=num[po]%100
-            modi=num[po]//100
-            n_in=[0,2,2,1,1,2,2,2,2,1][comm]
-            inp=[num.get([num[po+i+1],po+i+1,relb+num[po+i+1]] [(modi//10**i)%10],0) for i in range(n_in)]
+            modi=num[po]//10
             if comm<3 or comm==7 or comm==8:
-                store(2,[0,lambda a,b:a+b,lambda a,b:a*b,0,0,0,0,lambda a,b:a<b,lambda a,b:a==b][comm](inp[0],inp[1]))
+                store(3,[0,lambda a,b:a+b,lambda a,b:a*b,0,0,0,0,lambda a,b:a<b,lambda a,b:a==b][comm](load(1),load(2)))
                 po+=4
             elif comm==3:
                 if len(input)==0:
@@ -22,13 +22,13 @@ def genComp(program):
                 store(1,input.pop(0))
                 po+=2
             elif comm==4:
-                output.append(inp[0])
+                output.append(load(1))
                 po+=2
             elif comm==9:
-                relb+=inp[0]
+                relb+=load(1)
                 po+=2
             else:
-                po=[inp[1],po+3][(inp[0]==0)==(comm==5)]
+                po=[load(2),po+3][(load(1)==0)==(comm==5)]
         return True
     return comp
 
