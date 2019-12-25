cf6caaf243768e009dd0d80be50d07c0bc7346f6
Lythi45 <scala.bine@gmail.com>
2019-12-25 22:18:13 +0100

    day23

--- day23.py
+++ day23.py
@@ -1,11 +1,14 @@
+idle=[False]*50
+result=0
 def genComp(program):
     num={i:program[i] for i in range(len(program))}
-    pol=[0,0,0]
+    pol=[0,0,0,False]
     def comp(input):
         modi=0
         po=pol[0]
         relb=pol[1]
         co=pol[2]
+        idle=pol[3]
         #print('comp',input,po,relb)
         def store(off,value):
             #print('Store:',po+off,value)
@@ -25,14 +28,16 @@ def genComp(program):
                 #print('inp',input)
                 if len(input)>0:
                     store(1,input.pop(0))
+                    idle=False
                 else:
                     store(1,-1)
+                    idle=True
                 po+=2
             elif comm==4:
                 out=load(1)
                 po+=2
-                pol[0],pol[1],pol[2]=po,relb,co
-                return [out]
+                pol[0],pol[1],pol[2],pol[3]=po,relb,co,idle
+                return [out],idle
             elif comm==9:
                 relb+=load(1)
                 po+=2
@@ -45,9 +50,8 @@ def genComp(program):
                 print('Illegal Opcode',1/0)
             co+=1
             if co==100:
-                pol[0],pol[1],pol[2]=po,relb,0
-                return []
-        print('Final Score:',score)
+                pol[0],pol[1],pol[2],pol[3]=po,relb,0,idle
+                return [],idle
     return comp
 
 program=[int(i) for i in open("day23.txt", "r").read().split(',')]
@@ -57,6 +61,10 @@ dest=[0]*50
 state=[0]*50
 xpuf=[0]*50
 out=[]
+nat=False
+natx=0
+naty=0
+natset=set()
 for i in range(50):
     comps.append(genComp(program))
     input.append([i])
@@ -64,9 +72,9 @@ for i in range(50):
 while True:
     for i in range(50):
         #print('Comp:',i)
-        out=comps[i](input[i])
-        if len(out)>0:
-            print('out',i,out)
+        out,idle[i]=comps[i](input[i])
+        #if len(out)>0:
+        #    print('out',i,out)
         for n in out:
             if state[i]==0:
                 dest[i]=n
@@ -74,8 +82,20 @@ while True:
                 xpuf[i]=n
             else:
                 if dest[i]==255:
-                    print('result:',n)
-                    print(1/0)
-                input[dest[i]].append(xpuf[i])
-                input[dest[i]].append(n)
+                    if result==0:
+                        result=n
+                    natx=xpuf[i]
+                    naty=n
+                    nat=True
+                else:
+                    input[dest[i]].append(xpuf[i])
+                    input[dest[i]].append(n)
             state[i]=(state[i]+1)%3
+    if nat and sum(idle)==50:
+        input[0]=[natx,naty]
+        #print('nn',natx,naty,natset)
+        if naty in natset:
+            print('Result Part 1:',result)
+            print('Double y in NAT',naty)
+            print(1/0)
+        natset.add(naty)
